cd8fc3810d9a2db843e321a77c2fd0d3174b0cca
EgorSys <31861436+EgorSys@users.noreply.github.com>
2020-09-10 13:09:40 +0300

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,1 +1,23 @@
-print('Hello')
+import argparse
+import os, time
+
+# Example of argparser and getting parse values
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--filename", help="The name of the file to be renamed")
+parser.add_argument("-r", "--rename", help="The new name of the file")
+parser.add_argument("-m", "--mtime", help="Modification time", action="store_true")
+parser.add_argument("-s", "--size", help="The size of the file", action="store_true")
+
+args = parser.parse_args()
+
+filename = args.filename
+new_name = args.rename
+
+stats = os.stat(filename)
+if args.mtime:
+    print('Last modified:', time.ctime(stats.st_mtime))
+    
+if args.size:
+    print(f'File size: {(stats.st_size)//(2**20)} MB')
+
+os.rename(filename, new_name)
